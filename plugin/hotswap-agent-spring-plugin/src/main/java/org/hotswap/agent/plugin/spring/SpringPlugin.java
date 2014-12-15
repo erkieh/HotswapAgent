ac4395d4387724b4b6d3a1a9e3dd68f8ab90b96a
erkieh <erki.ehtla@gmail.com>
2014-12-15 23:04:10 +0200

    disable Spring changes analyzer due to possible relation to JVM locking/hanging

--- plugin/hotswap-agent-spring-plugin/src/main/java/org/hotswap/agent/plugin/spring/SpringPlugin.java
+++ plugin/hotswap-agent-spring-plugin/src/main/java/org/hotswap/agent/plugin/spring/SpringPlugin.java
@@ -72,15 +72,12 @@ public class SpringPlugin {
      */
     public void registerComponentScanBasePackage(final String basePackage) {
         LOGGER.info("Registering basePackage {}", basePackage);
-        final SpringChangesAnalyzer analyzer = new SpringChangesAnalyzer(appClassLoader);
         hotswapTransformer.registerTransformer(appClassLoader, basePackage + ".*", new ClassFileTransformer() {
             @Override
             public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
                 if (classBeingRedefined != null) {
-                    if (analyzer.isReloadNeeded(classBeingRedefined, classfileBuffer)) {
                         scheduler.scheduleCommand(new ClassPathBeanRefreshCommand(classBeingRedefined.getClassLoader(),
                             basePackage, className, classfileBuffer));
-                    }
                 }
                 return classfileBuffer;
             }
